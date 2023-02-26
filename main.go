af750332ae396324a6d8706f75c2b071e3148fad
Adrián Bíro <biroadrian@protonmail.com>
2023-02-26 10:31:02 +0100

    small fixes

--- main.go
+++ main.go
@@ -2,27 +2,39 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
-	"strings"
+	"time"
 
-	ical "github.com/arran4/golang-ical"
+	ics "github.com/arran4/golang-ical"
 )
 
 func main() {
-	b, err := os.ReadFile("sample.ics")
-	if err != nil {
-		log.Fatal(err)
-	}
-	cal, err := ical.ParseCalendar(strings.NewReader(string(b)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	// full file
+	/*
+		b, err := os.ReadFile("sample.ics")
+		if err != nil {
+			log.Fatal(err)
+		}
+		cal, err := ics.ParseCalendar(strings.NewReader(string(b)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		// full file
+		fmt.Println(cal1.Serialize())
+	*/
+	//
+	cal := ics.NewCalendar()
+	cal.SetMethod(ics.MethodRequest)
+	event := cal.AddEvent(fmt.Sprintf("id@domain"))
+	event.SetCreatedTime(time.Now())
+	event.SetDtStampTime(time.Now())
+	event.SetModifiedAt(time.Now())
+	event.SetStartAt(time.Now())
+	event.SetEndAt(time.Now())
+	event.SetSummary("Summary bum bum")
+	event.SetLocation("Address Kapurkova 5")
+	event.SetDescription("Description foo bar hola hej asdasdasd987987987")
+	event.SetURL("https://google.com")
+	event.AddRrule(fmt.Sprintf("FREQ=YEARLY;BYMONTH=%d;BYMONTHDAY=%d", time.Now().Month(), time.Now().Day()))
+	event.SetOrganizer("sender@domain", ics.WithCN("This Machine"))
+	event.AddAttendee("reciever or participant", ics.CalendarUserTypeIndividual, ics.ParticipationStatusNeedsAction, ics.ParticipationRoleReqParticipant, ics.WithRSVP(true))
 	fmt.Println(cal.Serialize())
-
-	for _, v := range cal.Events() {
-		fmt.Println(v)
-
-	}
 }
